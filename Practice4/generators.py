56d972d3de05d1a936cc98a17d116a077ea286f5
merrei24 <me_bisenbai@kbtu.kz>
2026-02-27 02:09:10 +0500

    generators.py

--- Practice4/generators.py
+++ Practice4/generators.py
@@ -8,24 +8,24 @@ for x in squares(5):
     print(x)
 
 # 2 Even numbers
-def evens(n):
+def evens(n):                #even numbers from 0 to n.
     for i in range(n + 1):
         if i % 2 == 0:
             yield i
 
-print(",".join(map(str, evens(20))))
+print(",".join(map(str, evens(20))))          #printed in one line separated by commas.
 
 # 3 Divisible by 3 and 4
-def div(n):
+def div(n):                  #numbers from 0 to n that are divisible by both 3 and 4.
     for i in range(n + 1):
         if i % 3 == 0 and i % 4 == 0:
             yield i 
 
-print(list(div(50)))
+print(list(div(50)))        #list
 
 # 4. Squares from a to b
 
-def squares(a, b):
+def squares(a, b):               #square of numbers between a and b.
     for i in range(a, b + 1):
         yield i * i
 
@@ -34,7 +34,7 @@ for value in squares(3, 7):
 
 # 5.Countdown from n to 0
 
-def countdown(n):
+def countdown(n):                 #counts backward from n to 0.
     for i in range(n, -1, -1):
         yield i
 
