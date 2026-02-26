320366747e419ba9488a9bc57ce277662f660757
merrei24 <me_bisenbai@kbtu.kz>
2026-02-27 01:35:40 +0500

    date.py

--- Practice4/date.py
+++ Practice4/date.py
@@ -3,7 +3,7 @@
 from datetime import datetime, timedelta
 
 now = datetime.now()
-print(now - timedelta(days=5))
+print(now - timedelta(days=5))     #prints date and time from 5 days ago
 
 # 2. Yesterday, Today, Tomorrow
 
@@ -11,20 +11,20 @@ from datetime import date, timedelta
 
 today = date.today()
 print(today - timedelta(days=1))
-print(today)
+print(today)            
 print(today + timedelta(days=1))
 
 # 3 Drop microseconds
 
 from datetime import datetime
 
-dt = datetime.now()
+dt = datetime.now()     #removes microseconds
 print(dt.replace(microsecond=0))
 
 # 4. Difference in seconds
-from datetime import datetime
+from datetime import datetime 3 #finds difference between two dates
 
 d1 = datetime(2024, 1, 1)
 d2 = datetime(2024, 1, 10)
 
-print((d2 - d1).total_seconds())
+print((d2 - d1).total_seconds()) #convert into seconds
