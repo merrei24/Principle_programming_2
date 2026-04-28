8d801446308d30428cebb1be03c1c1a4b57ef32e
merrei24 <me_bisenbai@kbtu.kz>
2026-04-29 03:02:10 +0500

    Update config.py

--- TSIS 1/config.py
+++ TSIS 1/config.py
@@ -1,5 +1,5 @@
 # config.py
 host = "localhost"
-database = "phonebook_db" # Өз дерекқорыңның аты
-user = "postgres"         # Әдетте postgres
-password = "postgres123" # Өз пароліңді жаз
+database = "phonebook_db" 
+user = "postgres"         
+password = "postgres123" 
